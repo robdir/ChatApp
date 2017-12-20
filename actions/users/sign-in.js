7393ec0021769c3b1d6fdcb7b0c7ff372ccb495d
robdir <rhwickham@gmail.com>
2017-12-20 15:16:50 +0100

    sign-in action

---
+++ actions/users/sign-in.js
@@ -0,0 +1,31 @@
+import API from '../../lib/api';
+import loadUser from './load';
+import {
+  API_LOADING,
+  API_READY,
+  API_ERROR
+} from '../../middleware/api';
+
+export const USER_SIGNED_IN = 'USER_SIGNED_IN';
+export const USER_AUTH_ERROR = 'USER_AUTH_ERROR';
+const api = new API();
+const users = api.service('users');
+
+
+export default () => {
+  return (dispatch) => {
+    dispatch({ type: API_LOADING });
+    api.authenticate(user)
+      .then((result) => {
+        dispatch({ type: API_READY });
+        dispatch({
+          type: USER_SIGNED_IN,
+          payload: result.data
+        });
+      })
+      .catch((error) => {
+        dispatch({ type: API_ERROR, payload: error });
+        dispatch({ type: USER_AUTH_ERROR, payload: error });
+    })
+  }
+}
